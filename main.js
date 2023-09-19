8ee7a6260d290ddea240a492424afcdc87f40e14
keyla_anguiano <keyla_anguiano@hotmail.com>
2023-09-18 20:41:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+  cursor: "<span style='color: #2dc4c6'>|</span>"
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color: #2dc4c6">Estudiante de Ingeniería en Sistemas Computacionales.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
